ffbc9f2573f970a310f26f50fef7c1407198b568
harolditoc <harold.chambilla@usil.pe>
2023-04-13 08:46:58 -0500

    Control

--- src/Controller/PromotorController.php
+++ src/Controller/PromotorController.php
@@ -14,10 +14,7 @@ use Endroid\QrCode\Color\Color;
 use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelLow;
 use Endroid\QrCode\QrCode;
-use Endroid\QrCode\Label\Label;
-use Endroid\QrCode\Logo\Logo;
 use Endroid\QrCode\Writer\PngWriter;
-use Endroid\QrCode\Label\Font\NotoSans;
 use Knp\Snappy\Image;
 
 class PromotorController extends AbstractController
